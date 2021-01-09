4a76a2d44d9920c5c503ead5cbe56f3b9017ecee
cesar-machado <cesarmach19@gmail.com>
2021-01-09 12:28:18 -0300

    lesson 6,7 python

---
+++ loops.py
@@ -0,0 +1,15 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+for i in range(6):
+    print(i)
+
+
+names = ["cesar", "gil", "ben"]
+
+for name in names:
+    print(name)
+
+name1 = "cesar"
+for character in name1:
+    print(character)
